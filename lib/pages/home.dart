bd2d18a4713f62048709940c55b07137a3e6a982
Aakrit Subedi <aakritsubedi9@gmail.com>
2020-11-01 11:07:21 +0545

    Replica of dummy task and minor design fixes

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -19,41 +19,54 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Container(
             color: Color(0xFFF6F6F6),
             width: double.infinity,
-            padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 24.0),
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
             child: Stack(
               children: [
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Container(
-                      margin: EdgeInsets.only(bottom: 32.0),
+                      margin: EdgeInsets.only(
+                        bottom: 32.0,
+                        top: 24.0
+                      ),
                       child: Image(
                         image: AssetImage('assets/images/logo.png'),
                       ),
                     ),
-                    TaskCardWidget(
-                        title: 'Get Started!!',
-                        desc:
-                            'Hello boss!! Welcome to Todoist app, this is a default task that you can edit or delete to get started.'),
-                    TaskCardWidget(
-                      title: 'Your second task',
-                    )
+                    Expanded(
+                        child: ListView(
+                      children: [
+                        TaskCardWidget(
+                            title: 'Get Started!!',
+                            desc:
+                                'Hello boss!! Welcome to Todoist app, this is a default task that you can edit or delete to get started.'),
+                        TaskCardWidget(
+                          title: 'Your second task',
+                        ),
+                        TaskCardWidget(
+                            title: 'Get Started!!',
+                            desc:
+                                'Hello boss!! Welcome to Todoist app, this is a default task that you can edit or delete to get started.'),
+                        TaskCardWidget(
+                          title: 'Your second task',
+                        )
+                      ],
+                    ))
                   ],
                 ),
                 Positioned(
-                  bottom: 0.0,
+                  bottom: 24.0,
                   right: 0.0,
                   child: Container(
-                    height: 50.0,
-                    width: 50.0,
-                    decoration: BoxDecoration(
-                      color: Color(0xFF02e7ea),
-                      borderRadius: BorderRadius.circular(25.0)
-                    ),
-                    child: Image(
-                      image: AssetImage('assets/images/add_icon.png'),
-                    )
-                  ),
+                      height: 50.0,
+                      width: 50.0,
+                      decoration: BoxDecoration(
+                          color: Color(0xFF049fbfd),
+                          borderRadius: BorderRadius.circular(25.0)),
+                      child: Image(
+                        image: AssetImage('assets/images/add_icon.png'),
+                      )),
                 )
               ],
             ),
